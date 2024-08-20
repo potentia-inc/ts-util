553e287aef8009a55166e21526c61e6df44ccc14
Tz-Huan Huang <tzhuan@potentia.asia>
2024-08-20 10:20:43 +0800

    Support URL with credentials for request()

--- src/request.ts
+++ src/request.ts
@@ -1,4 +1,5 @@
 import { NIL } from './type.js'
+
 export async function request(
   url: string,
   options: RequestInit & { timeout?: number } = {},
@@ -6,5 +7,24 @@ export async function request(
   const { timeout, ...init } = options
   if (timeout !== NIL && timeout !== null)
     init.signal = AbortSignal.timeout(timeout)
-  return await fetch(url, init)
+  const _url = new URL(url)
+  const { username, password } = _url
+  _url.username = ''
+  _url.password = ''
+  const authorization =
+    username === '' && password === ''
+      ? NIL
+      : `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`
+  return await fetch(
+    _url.toString(),
+    authorization === NIL
+      ? init
+      : {
+          ...init,
+          headers: {
+            ...(init.headers ?? {}),
+            authorization,
+          },
+        },
+  )
 }
--- src/type.ts
+++ src/type.ts
@@ -4,7 +4,6 @@ declare global {
   }
 }
 
-// eslint-disable-next-line no-extend-native
 BigInt.prototype.toJSON = function () {
   return this.toString()
 }
